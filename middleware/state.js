0e1681531e0a31b8f1d7eb7ea9a8859637d9faba
Daniel W. Hieber <dwhieb@gmail.com>
2022-02-16 20:59:57 -0700

    add Help Menu

--- middleware/state.js
+++ middleware/state.js
@@ -1,11 +1,5 @@
 export default function middleware(ctx, next) {
-
-  const state = {
-    meta: ctx.meta,
-  };
-
-  Object.assign(ctx.state, state);
-
+  Object.assign(ctx.state, ctx.context);
+  delete ctx.context;
   return next();
-
 }
