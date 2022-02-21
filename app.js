ccb9ff10d0e697ccc5e5f34c257e8244ab5f6eaa
Daniel W. Hieber <dwhieb@gmail.com>
2022-02-20 23:12:58 -0700

    Koa > Express

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
-import Koa from 'koa';
-import { loadContext, render, state } from './middleware/index.js';
+import express           from 'express'
+import { fileURLToPath } from 'url'
+import { getLocals }     from './middleware/index.js'
+import hbs               from './config/handlebars.js'
+import path              from 'path'
 
-/**
- * Returns the same response for every request.
- */
-function listener(ctx) {
-  if (ctx.url.includes(`languages`)) ctx.render(`Languages`);
-  else ctx.render(`Home`);
+import { env, port } from './config/app.js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname  = path.dirname(__filename)
+
+function serverCallback() {
+  console.info(`Server started on port ${ port } in ${ env } mode. Press Ctrl+C to terminate.`)
 }
 
-// Initialize Koa
-const app = new Koa();
+// Initialize app
+const app = express()
 
 // Settings
-loadContext(app.context);
-app.env   = process.env.NODE_ENV ?? `localhost`;
-app.port  = process.env.PORT ?? 3001;
-app.proxy = true; // trust the Azure proxy
+app.enable(`trust proxy`)
+app.engine(`hbs`, hbs.engine)
+app.set(`env`, env)
+app.set(`view engine`, `hbs`)
+app.set(`views`, path.resolve(__dirname, `./pages`))
 
-// Middleware
-app
-.use(state)
-.use(render)
-.use(listener);
+// Locals
+await getLocals(app.locals)
+
+// Routing
+app.get(`/`, (req, res) => {
+  res.render(`Home`)
+})
+
+app.get(`/languages`, (req, res) => {
+  res.render(`Languages`)
+})
 
 // Start server
-app.listen(app.port, () => console.info(`Server started on port ${ app.port } in ${ app.env } mode. Press Ctrl+C to terminate.`));
+app.listen(port, serverCallback)
--- middleware/index.js
+++ middleware/index.js
@@ -1,3 +1,1 @@
-export { default as loadContext } from './context.js';
-export { default as render } from './render.js';
-export { default as state } from './state.js';
+export { default as getLocals } from './locals.js'
