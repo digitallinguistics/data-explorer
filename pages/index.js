1d1ecf9064d574969f14fd04dfd4a79c27a20826
Tong Jiao <71759137+TongJ05@users.noreply.github.com>
2022-03-07 16:40:30 -0600

    Add a dictionaries page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
-import Home        from './Home/Home.js'
-import Languages   from './Languages/Languages.js'
-import NotFound    from './404/404.js'
-import ServerError from './500/500.js'
+import Dictionaries from './Dictionaries/Dictionaries.js'
+import Home         from './Home/Home.js'
+import Languages    from './Languages/Languages.js'
+import NotFound     from './404/404.js'
+import ServerError  from './500/500.js'
 
 export default {
   Home,
   Languages,
   NotFound,
   ServerError,
+  Dictionaries,
 }
