6c12099b6badf1b34266ed35d58bf9ee77c0d80a
Tong Jiao <71759137+TongJ05@users.noreply.github.com>
2022-03-08 18:37:19 -0600

    NEW: Projects page (#6)
    closes #6

--- app/routes.js
+++ app/routes.js
@@ -2,8 +2,9 @@ import handlers from '../pages/index.js'
 
 export default function addRoutes(app) {
   app.get(`/`, handlers.Home)
-  app.get(`/languages`, handlers.Languages)
   app.get(`/about`, handlers.About)
+  app.get(`/languages`, handlers.Languages)
+  app.get(`/projects`, handlers.Projects)
   app.use(handlers.NotFound)
   app.use(handlers.ServerError)
 }
