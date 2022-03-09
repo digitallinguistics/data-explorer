8b874594d30a1adc9a0dc6fe3668477e7918ac86
Daniel W. Hieber <dwhieb@gmail.com>
2022-03-08 17:27:41 -0700

    sort pages

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
+import About       from './About/About.js'
 import Home        from './Home/Home.js'
 import Languages   from './Languages/Languages.js'
 import NotFound    from './404/404.js'
 import ServerError from './500/500.js'
-import About       from './About/About.js'
 
 export default {
+  About,
   Home,
   Languages,
   NotFound,
   ServerError,
-  About,
 }
