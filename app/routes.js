d9e590e50748b928d29a403a7459403d35a5f339
Tong Jiao <71759137+TongJ05@users.noreply.github.com>
2022-03-08 18:44:42 -0600

    NEW: Dictionaries page (#34)
    closes #34

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,7 @@ import handlers from '../pages/index.js'
 export default function addRoutes(app) {
   app.get(`/`, handlers.Home)
   app.get(`/about`, handlers.About)
+  app.get(`/dictionaries`, handlers.Dictionaries)
   app.get(`/languages`, handlers.Languages)
   app.get(`/projects`, handlers.Projects)
   app.use(handlers.NotFound)
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
-import About       from './About/About.js'
-import Home        from './Home/Home.js'
-import Languages   from './Languages/Languages.js'
-import NotFound    from './404/404.js'
-import Projects    from './Projects/Projects.js'
-import ServerError from './500/500.js'
+import About        from './About/About.js'
+import Dictionaries from './Dictionaries/Dictionaries.js'
+import Home         from './Home/Home.js'
+import Languages    from './Languages/Languages.js'
+import NotFound     from './404/404.js'
+import Projects     from './Projects/Projects.js'
+import ServerError  from './500/500.js'
 
 export default {
   About,
+  Dictionaries,
   Home,
   Languages,
   NotFound,
