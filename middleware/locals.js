2e67a9972b14617ec4b947997bf73f5ae2feab20
Daniel W. Hieber <dwhieb@gmail.com>
2022-03-11 20:38:50 -0700

    Revert "inline critical CSS"
    This reverts commit 866080e97289ce6bf4f3e1d09268ccb46cb808ad.

--- middleware/locals.js
+++ middleware/locals.js
@@ -2,9 +2,6 @@ import { fileURLToPath } from 'url'
 import path              from 'path'
 import { readFile }      from 'fs/promises'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname  = path.dirname(__filename)
-
 function createBugReportQuery() {
 
   const params = {
@@ -14,11 +11,6 @@ function createBugReportQuery() {
 
   return new URLSearchParams(params).toString()
 
-}
-
-function getCriticalCSS() {
-  const layoutCSSPath = path.join(__dirname, `../layout/layout.css`)
-  return readFile(layoutCSSPath, `utf8`)
 }
 
 function createFeatureRequestQuery() {
@@ -33,15 +25,16 @@ function createFeatureRequestQuery() {
 }
 
 async function getMetadata() {
+  const __filename = fileURLToPath(import.meta.url)
+  const __dirname = path.dirname(__filename)
   const metaPath = path.join(__dirname, `../package.json`)
-  const json     = await readFile(metaPath, `utf8`)
+  const json = await readFile(metaPath, `utf8`)
   return JSON.parse(json)
 }
 
 export default async function addLocals(locals) {
   Object.assign(locals, {
     bugReportQuery:      createBugReportQuery(),
-    criticalCSS:         await getCriticalCSS(),
     featureRequestQuery: createFeatureRequestQuery(),
     meta:                await getMetadata(),
   })
