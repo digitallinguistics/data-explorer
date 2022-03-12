0812090f4601f9e544c0a9a9131424f3e72109a7
Daniel W. Hieber <dwhieb@gmail.com>
2022-03-12 15:54:18 -0700

    NEW: login / logout (#52, #53, #113, #114, #141)
    closes #52
    closes #53
    closes #113
    closes #114
    closes #141

--- pages/500/500.js
+++ pages/500/500.js
@@ -3,7 +3,11 @@
   no-unused-vars,
 */
 
+import { env } from '../../config/app.js'
+
 export default function get(err, req, res, next) {
+
+  if (env !== `production`) console.error(err)
 
   const title = `500`
 
