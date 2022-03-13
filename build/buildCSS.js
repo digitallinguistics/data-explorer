a72e7146f48075f11a2aa240de75374a4655891f
Daniel W. Hieber <dwhieb@gmail.com>
2022-03-12 17:18:05 -0700

    use Handlebars partial for critical CSS

--- build/buildCSS.js
+++ build/buildCSS.js
@@ -6,13 +6,14 @@ import { readFile, writeFile } from 'fs/promises'
 
 export default async function buildCSS() {
 
-  const __filename     = fileURLToPath(import.meta.url)
-  const __dirname      = path.dirname(__filename)
-  const layoutLESSPath = path.join(__dirname, `../layout/index.less`)
-  const layoutCSSPath  = path.join(__dirname, `../layout/index.css`)
-  const less           = await readFile(layoutLESSPath, `utf8`)
-  const css            = await convertLESS(less)
+  const __filename        = fileURLToPath(import.meta.url)
+  const __dirname         = path.dirname(__filename)
+  const layoutLESSPath    = path.join(__dirname, `../layout/index.less`)
+  const layoutPartialPath = path.join(__dirname, `../layout/style.hbs`)
+  const less              = await readFile(layoutLESSPath, `utf8`)
+  const css               = await convertLESS(less)
+  const html              = `<style>${ css }</style>`
 
-  await writeFile(layoutCSSPath, css, `utf8`)
+  await writeFile(layoutPartialPath, html, `utf8`)
 
 }
