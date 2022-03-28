506001b0b16e49601f1abfcab57b66fb8b098dbf
Daniel W. Hieber <dwhieb@gmail.com>
2022-03-27 19:42:58 -0600

    add build step for page styles

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,17 @@
-import { msAuthCookie, msAuthHeader } from '../constants/index.js'
+import { env } from '../config/app.js'
+import { msAuthCookie, msAuthHeader, msAuthUser } from '../constants/index.js'
 
 export default function auth(req, res, next) {
 
   const authCookie = req.cookies[msAuthCookie]
   res.locals.loggedIn = Boolean(authCookie)
 
+  if (env === `localhost` && res.locals.loggedIn) {
+    req.headers[msAuthHeader] = msAuthUser
+  }
+
   if (res.locals.loggedIn) {
-    res.locals.user = req.get(msAuthHeader)
+    res.locals.user = req.headers[msAuthHeader]
   }
 
   next()
