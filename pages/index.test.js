b13b37d97ca1cd9c0b432651e5c37cc0ecdd0a32
Tong Jiao <71759137+TongJ05@users.noreply.github.com>
2022-03-28 20:31:00 -0500

    TEST: main nav (#142)
    closes #142

--- pages/index.test.js
+++ pages/index.test.js
@@ -2,6 +2,22 @@ describe(`layout`, function() {
 
   describe(`Main Nav`, function() {
 
+    it(`About`, function() {
+      cy.visit(`/`)
+      cy.contains(`.navbar a`, `About`)
+      .click()
+      cy.url().should(`include`, `/about`)
+      cy.contains(`h1`, `About`)
+    })
+
+    it(`Dictionaries`, function() {
+      cy.visit(`/`)
+      cy.contains(`.navbar a`, `Dictionaries`)
+      .click()
+      cy.url().should(`include`, `/dictionaries`)
+      cy.contains(`h1`, `Dictionaries`)
+    })
+
     it(`Home`, function() {
       cy.visit(`/languages`)
       cy.contains(`.navbar a`, `Home`)
@@ -15,6 +31,14 @@ describe(`layout`, function() {
       .click()
       cy.url().should(`include`, `/languages`)
       cy.contains(`h1`, `Languages`)
+    })
+
+    it(`Projects`, function() {
+      cy.visit(`/`)
+      cy.contains(`.navbar a`, `Projects`)
+      .click()
+      cy.url().should(`include`, `/projects`)
+      cy.contains(`h1`, `Projects`)
     })
 
   })
