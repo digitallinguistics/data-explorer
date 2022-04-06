adb75c7162c9059c4ba37649cfd53c4e19a70b5e
Daniel W. Hieber <dwhieb@gmail.com>
2022-04-06 15:30:40 -0600

    NEW: Languages List (#185)
    closes #185

--- config/handlebars.js
+++ config/handlebars.js
@@ -1,17 +1,39 @@
 import { ExpressHandlebars } from 'express-handlebars'
 import { fileURLToPath }     from 'url'
+import getDefaultLanguage    from '../utilities/getDefaultLanguage.js'
 import path                  from 'path'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
+/**
+ * Renders the given analysis language for a MultiLangString.
+ * @param {MultiLangString} string   The MultiLangString object to render.
+ * @param {String}          language The language to render the MLS in.
+ * @returns
+ */
+function mls(string, language) {
+  return getDefaultLanguage(string, language)
+}
+
+function section(name, opts) {
+  this.sections ??= {}
+  this.sections[name] = opts.fn(this)
+  return null
+}
+
 const hbs = new ExpressHandlebars({
   defaultLayout: `index`,
   extname:       `hbs`,
+  helpers:       {
+    mls,
+    section,
+  },
   layoutsDir:    path.resolve(__dirname, `../layout`),
   partialsDir:   [
     path.resolve(__dirname, `../components`),
     path.resolve(__dirname, `../layout`),
+    path.resolve(__dirname, `../pages`),
   ],
 })
 
