ea837f409ff5420cf640ec1ff2f4f9404c69a448
Daniel W. Hieber <dwhieb@gmail.com>
2022-04-26 19:57:17 -0600

    IN PROGRESS: Language Details

--- pages/Design/Design.js
+++ pages/Design/Design.js
@@ -1,10 +1,23 @@
-export default function get(req, res) {
+import { fileURLToPath }  from 'url'
+import path               from 'path'
+import { readFile }       from 'fs/promises'
+import yaml               from 'js-yaml'
+
+export default async function get(req, res) {
+
+  const __filename    = fileURLToPath(import.meta.url)
+  const __dirname     = path.dirname(__filename)
+  const languagesPath = path.join(__dirname, `../../data/languages.yml`)
+  const languagesYAML = await readFile(languagesPath, `utf8`)
+  const languages     = yaml.load(languagesYAML)
+  const language      = languages.pop()
 
   const title         = `Design`
   const { component } = req.params
 
   res.render(`Design/Design`, {
     [component]: true,
+    language,
     [title]:     true,
     title,
   })
