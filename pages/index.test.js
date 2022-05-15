30a002046f70939694c9296b698d33bb50af8654
Daniel W. Hieber <dwhieb@gmail.com>
2022-05-15 15:36:01 -0600

    FIX: Main Nav tests

--- pages/index.test.js
+++ pages/index.test.js
@@ -2,22 +2,6 @@ describe(`layout`, function() {
 
   describe(`Main Nav`, function() {
 
-    it(`About`, function() {
-      cy.visit(`/`)
-      cy.contains(`.navbar a`, `About`)
-      .click()
-      cy.url().should(`include`, `/about`)
-      cy.contains(`h1`, `About`)
-    })
-
-    it(`Dictionaries`, function() {
-      cy.visit(`/`)
-      cy.contains(`.navbar a`, `Dictionaries`)
-      .click()
-      cy.url().should(`include`, `/dictionaries`)
-      cy.contains(`h1`, `Dictionaries`)
-    })
-
     it(`Home`, function() {
       cy.visit(`/languages`)
       cy.contains(`.navbar a`, `Home`)
@@ -31,14 +15,6 @@ describe(`layout`, function() {
       .click()
       cy.url().should(`include`, `/languages`)
       cy.contains(`h1`, `Languages`)
-    })
-
-    it(`Projects`, function() {
-      cy.visit(`/`)
-      cy.contains(`.navbar a`, `Projects`)
-      .click()
-      cy.url().should(`include`, `/projects`)
-      cy.contains(`h1`, `Projects`)
     })
 
   })
