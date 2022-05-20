a674ff218a6f065599dfc5f5c0ce63090d5e4f67
Daniel W. Hieber <dwhieb@gmail.com>
2022-05-20 15:06:27 -0600

    rename generated JS files after build

--- build/buildJS.js
+++ build/buildJS.js
@@ -4,22 +4,29 @@ import { env }                       from '../config/app.js'
 import { esbuildPluginBrowserslist } from 'esbuild-plugin-browserslist'
 import { fileURLToPath }             from 'url'
 import path                          from 'path'
+import recurse                       from 'readdirp'
+import { rename }                    from 'fs/promises'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname  = path.dirname(__filename)
 const pagesDir   = path.join(__dirname, `../pages`)
+const scriptsDir = path.join(__dirname, `../public/scripts`)
 
 const config = {
   bundle:      true,
   entryPoints: [path.join(pagesDir, `Lexemes/Lexemes-client.js`)],
   format:      `esm`,
   minify:      env === `production`,
-  outdir:      path.join(__dirname, `../public/scripts`),
+  outdir:      scriptsDir,
   plugins:     [esbuildPluginBrowserslist(browserslist())],
   sourcemap:   env === `production` ? true : `inline`,
 }
 
-async function renameFiles() {}
+async function renameFiles() {
+  for await (const entry of recurse(scriptsDir)) {
+    await rename(entry.fullPath, entry.fullPath.replace(`-client`, ``))
+  }
+}
 
 export default async function buildJS() {
   await build(config)
