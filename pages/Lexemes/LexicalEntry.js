db7f9a4030635603b7745ebcd58a8f46419ddc71
Daniel W. Hieber <dwhieb@gmail.com>
2022-05-20 16:12:53 -0600

    add interactivity to tablist

--- pages/Lexemes/LexicalEntry.js
+++ pages/Lexemes/LexicalEntry.js
@@ -1,1 +1,16 @@
-console.log(`This is the Lexical Entry script.`)
+const tablist = document.querySelector(`.tabs`)
+
+function setCurrentTab(ev) {
+
+  const links = document.querySelectorAll(`.tabs a`)
+
+  for (const link of links) {
+    link.removeAttribute(`aria-selected`)
+  }
+
+  ev.target.setAttribute(`aria-selected`, true)
+
+}
+
+location.hash = `form` // initialize to Form tab
+tablist.addEventListener(`click`, setCurrentTab)
