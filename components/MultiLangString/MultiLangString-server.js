4bfe73f2e3d918e4102bbd311c01a442c8a4f30f
Daniel W. Hieber <dwhieb@gmail.com>
2022-07-25 17:40:06 -0600

    update MLS test data

--- components/MultiLangString/MultiLangString-server.js
+++ components/MultiLangString/MultiLangString-server.js
@@ -1,7 +1,11 @@
 export default function get(req, res) {
   res.render(`Component/Component`, {
     component: `MultiLangString/MultiLangString`,
-    data:      { eng: `hello` },
+    data:      {
+      eng: `Spanish`,
+      fra: `espagnol`,
+      spa: `español`,
+    },
     layout:    false,
     styles:    [`MultiLangString`],
   })
