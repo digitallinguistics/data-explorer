994f1611db17eeab093490b930be7fa50de35a21
Daniel W. Hieber <dwhieb@gmail.com>
2022-11-07 12:56:31 -0700

    display project permissions

--- pages/Projects/Projects-server.js
+++ pages/Projects/Projects-server.js
@@ -1,6 +1,11 @@
-import compare       from '../../utilities/compare.js'
-import db            from '../../config/database.js'
-import { hasAccess } from '../../utilities/permissions.js'
+import compare from '../../utilities/compare.js'
+import db      from '../../config/database.js'
+
+import {
+  hasAccess,
+  isEditor,
+  isOwner,
+} from '../../utilities/permissions.js'
 
 export default async function get(req, res) {
 
@@ -10,6 +15,11 @@ export default async function get(req, res) {
 
   projects = projects.filter(project => hasAccess(res.locals.user, project))
   projects.sort((a, b) => compare(a.name, b.name))
+
+  for (const project of projects) {
+    project.permissions.isOwner  = isOwner(res.locals.user, project)
+    project.permissions.isEditor = isEditor(res.locals.user, project)
+  }
 
   res.render(`Projects/Projects`, {
     projects,
