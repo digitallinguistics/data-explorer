f12e8e7901def0d184338a2d83813aadccadce00
Daniel W. Hieber <dwhieb@gmail.com>
2022-11-11 21:07:56 -0700

    FIX: unit tests

--- pages/Languages/Languages.test.js
+++ pages/Languages/Languages.test.js
@@ -1,27 +1,41 @@
+import { hasAccess }    from '../../utilities/permissions.js'
 import { msAuthCookie } from '../../constants/index.js'
+import yamlParser       from 'js-yaml'
 
 describe(`Languages`, function() {
+
+  before(function() {
+    cy.readFile(`data/languages.yml`)
+    .then(yaml => yamlParser.load(yaml))
+    .as(`languages`)
+  })
 
   describe(`/languages`, function() {
 
     it(`displays all public languages`, function() {
+
+      const publicLanguages = this.languages.filter(language => language.permissions.public)
+
       cy.visit(`/languages`)
       cy.title().should(`eq`, `Oxalis | Languages`)
       cy.get(`.page-title`).should(`have.text`, `Languages | All`)
       cy.get(`.languages-table caption`).should(`have.text`, `Languages | All`)
-      cy.get(`.languages-table tbody`).children().should(`have.length`, 4)
+      cy.get(`.languages-table tbody`).children().should(`have.length`, publicLanguages.length)
       cy.contains(`td`, `Chitimacha`).should(`not.exist`)
       cy.contains(`td`, `Swahili`).should(`not.exist`)
       cy.get(`tbody .privacy`).each(td => expect(td).to.contain(`public`))
       cy.get(`tbody .permissions`).each(td => expect(td).to.contain(`public`))
+
     })
 
     it(`displays all private languages the user has permission to access (owner@digitallinguistics.io)`, function() {
 
+      const ownerLanguages = this.languages.filter(language => hasAccess(`owner@digitallinguistics.io`, language))
+
       cy.visit(`/languages`)
       cy.setCookie(msAuthCookie, `owner@digitallinguistics.io`)
       cy.visit(`/languages`)
-      cy.get(`.languages-table tbody`).children().should(`have.length`, 6)
+      cy.get(`.languages-table tbody`).children().should(`have.length`, ownerLanguages.length)
 
       cy.contains(`td`, `Chitimacha`).parent()
       .within(() => {
@@ -39,10 +53,12 @@ describe(`Languages`, function() {
 
     it(`displays all private languages the user has permission to access (editor@digitallinguistics.io)`, function() {
 
+      const editorLanguages = this.languages.filter(language => hasAccess(`editor@digitallinguistics.io`, language))
+
       cy.visit(`/languages`)
       cy.setCookie(msAuthCookie, `editor@digitallinguistics.io`)
       cy.visit(`/languages`)
-      cy.get(`.languages-table tbody`).children().should(`have.length`, 4)
+      cy.get(`.languages-table tbody`).children().should(`have.length`, editorLanguages.length)
 
       cy.contains(`td`, `Menominee`).parent()
       .within(() => {
@@ -60,10 +76,12 @@ describe(`Languages`, function() {
 
     it(`displays all private languages the user has permission to access (viewer@digitallinguistics.io)`, function() {
 
+      const viewerLanguages = this.languages.filter(language => hasAccess(`viewer@digitallinguistics.io`, language))
+
       cy.visit(`/languages`)
       cy.setCookie(msAuthCookie, `viewer@digitallinguistics.io`)
       cy.visit(`/languages`)
-      cy.get(`.languages-table tbody`).children().should(`have.length`, 5)
+      cy.get(`.languages-table tbody`).children().should(`have.length`, viewerLanguages.length)
 
       cy.contains(`td`, `Chitimacha`).parent()
       .within(() => {
@@ -102,13 +120,13 @@ describe(`Languages`, function() {
       cy.get(`.error-message`).should(`have.text`, `You do not have permission to view this project.`)
     })
 
-    it(`displays all (and only all) project languages`, function() {
+    it.only(`displays all (and only all) project languages`, function() {
 
       const publicProjectID = `c554474c-7f39-4ede-941b-c40b8f58b059` // Nisinoon
 
       cy.visit(`/projects/${ publicProjectID }/languages`)
       cy.title().should(`eq`, `Oxalis | Nisinoon | Languages`)
-      cy.get(`tbody`).children().should(`have.length`, 4)
+      cy.get(`tbody`).children().should(`have.length`, 5)
 
     })
 
