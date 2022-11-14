e2ff880ced003a01e001d8db991608930562b6fa
Daniel W. Hieber <dwhieb@gmail.com>
2022-11-14 14:52:29 -0700

    remove `.only()`

--- pages/Lexeme/Lexeme.test.js
+++ pages/Lexeme/Lexeme.test.js
@@ -42,7 +42,7 @@ describe(`Lexeme page`, function() {
     cy.get(`.error-message`).should(`have.text`, `You do not have permission to view this lexeme.`)
   })
 
-  it.only(`Lexeme Details: Arapaho: ‑(')enih`, function() {
+  it(`Lexeme Details: Arapaho: ‑(')enih`, function() {
 
     const lexemeID = `67944dcf-f7d9-4e9c-88f7-cb2408b10b9b`
 
