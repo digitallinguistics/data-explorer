506ba4547c6752708b172da3ba93a4ab9e88da04
Daniel W. Hieber <dwhieb@gmail.com>
2022-12-18 02:13:32 -0700

    show number of lexical entries

--- pages/Language/Language-server.js
+++ pages/Language/Language-server.js
@@ -28,11 +28,14 @@ export default async function get(req, res) {
   const { data: projects } = await db.getProjects()
   const languageProjects   = projects.filter(project => language.projects.includes(project.id) && hasAccess(res.locals.user, project))
 
+  const { data: { count: numEntries } } = await db.getLexemes({ language: languageID, summary: true })
+
   res.render(`Language/Language`, {
     language,
-    Language: true,
-    projects: languageProjects,
-    title:    getDefaultOrthography(language.name) ?? `Language`,
+    Language:   true,
+    numEntries,
+    projects:   languageProjects,
+    title:      getDefaultOrthography(language.name) ?? `Language`,
   })
 
 }
