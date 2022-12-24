bd9193c645f832c3b5de526cfc59f44583888b07
Daniel W. Hieber <dwhieb@gmail.com>
2022-12-24 16:02:49 -0700

    Languages tests

--- pages/Language/Language.test.js
+++ pages/Language/Language.test.js
@@ -17,14 +17,6 @@ describe(`Language Page`, function() {
     cy.task(`setupDatabase`)
   })
 
-  beforeEach(function() {
-    // It's necessary to do this in `beforeEach()` because Cypress clears aliases between each test.
-    // So any aliases created in `before()` only persist for the first test run after it.
-    cy.readFile(`data/language.yml`)
-    .then(yaml => yamlParser.load(yaml))
-    .as(`data`)
-  })
-
   afterEach(function() {
     cy.clearDatabase()
   })
@@ -70,84 +62,89 @@ describe(`Language Page`, function() {
 
   it(`Language Details`, function() {
 
-    const { data } = this
-    const count    = 3
+    cy.readFile(`data/language.yml`)
+    .then(yaml => yamlParser.load(yaml))
+    .then(data => {
 
-    cy.addOne(data)
+      cy.addOne(data)
 
-    cy.addOne(new Project({
-      id:   `683a5d27-53bf-451b-80d1-f6e731674c9e`,
-      name: `Chitimacha Dictionary Project`,
-    }))
+      cy.addOne(new Project({
+        id:   `683a5d27-53bf-451b-80d1-f6e731674c9e`,
+        name: `Chitimacha Dictionary Project`,
+      }))
 
-    cy.addOne(new Project({
-      id:   `225eeff5-adb8-421b-b9d0-7b5174419402`,
-      name: `Typology Project`,
-    }))
+      cy.addOne(new Project({
+        id:   `225eeff5-adb8-421b-b9d0-7b5174419402`,
+        name: `Typology Project`,
+      }))
 
-    cy.addMany(count, new Lexeme({
-      language: {
-        id: data.id,
-      },
-    }))
+      const count = 3
 
-    cy.visit(`/languages/${ data.id }`)
-    cy.title().should(`equal`, `Oxalis | ${ data.name.eng }`)
+      cy.addMany(count, new Lexeme({
+        language: {
+          id: data.id,
+        },
+      }))
 
-    // Page Title
-    cy.get(`.page-title`).should(`have.text`, data.name.eng)
+      cy.visit(`/languages/${ data.id }`)
+      cy.title().should(`equal`, `Oxalis | ${ data.name.eng }`)
 
-    // Scientific Name
-    for (const lang in data.name) {
-      cy.contains(`.name dt`, lang)
-      cy.contains(`.name dd`, data.name[lang])
-    }
+      // Page Title
+      cy.get(`.page-title`).should(`have.text`, data.name.eng)
 
-    // Autonym
-    for (const ortho in data.autonym) {
-      cy.contains(`.autonym dt`, ortho)
-      cy.contains(`.autonym dd`, data.autonym[ortho])
-    }
+      // Scientific Name
+      for (const lang in data.name) {
+        cy.contains(`.name dt`, lang)
+        cy.contains(`.name dd`, data.name[lang])
+      }
 
-    // Language Codes
-    cy.get(`.codes dd`)
-    .then(([glottocodeEl, isoEl, abbreviationEl]) => {
-      expect(glottocodeEl.textContent).to.include(data.glottocode)
-      expect(isoEl.textContent).to.include(data.iso)
-      expect(abbreviationEl.textContent).to.include(data.abbreviation)
+      // Autonym
+      for (const ortho in data.autonym) {
+        cy.contains(`.autonym dt`, ortho)
+        cy.contains(`.autonym dd`, data.autonym[ortho])
+      }
+
+      // Language Codes
+      cy.get(`.codes dd`)
+      .then(([glottocodeEl, isoEl, abbreviationEl]) => {
+        expect(glottocodeEl.textContent).to.include(data.glottocode)
+        expect(isoEl.textContent).to.include(data.iso)
+        expect(abbreviationEl.textContent).to.include(data.abbreviation)
+      })
+
+      // Description
+      cy.get(`.description`)
+      .should(`include.text`, data.description.markdown.slice(0, 20))
+
+      // METADATA
+
+      // URL
+      cy.contains(`#url`, `https://data.digitallinguistics.io/languages/${ data.id }`)
+
+      // Date Created
+      cy.contains(`#date-created`, new Date(data.dateCreated).toLocaleDateString(`en-CA`))
+
+      // Date Modified
+      cy.contains(`#date-modified`, new Date(data.dateModified).toLocaleDateString(`en-CA`))
+
+      // # of Lexical Entries
+      cy.contains(`#num-lexical-entries`, count)
+
+      // Projects
+      cy.get(`#projects`).children()
+      .should(`have.length`, data.projects.length)
+      .then(([a, b]) => {
+        expect(a).to.have.text(`Chitimacha Dictionary Project`)
+        expect(b).to.have.text(`Typology Project`)
+      })
+
+      // Notes
+      for (const note of data.notes) {
+        cy.contains(`.note__source`, note.source.abbreviation)
+        cy.contains(`.note__text`, note.text)
+      }
+
     })
-
-    // Description
-    cy.get(`.description`)
-    .should(`include.text`, data.description.markdown.slice(0, 20))
-
-    // METADATA
-
-    // URL
-    cy.contains(`#url`, `https://data.digitallinguistics.io/languages/${ data.id }`)
-
-    // Date Created
-    cy.contains(`#date-created`, new Date(data.dateCreated).toLocaleDateString(`en-CA`))
-
-    // Date Modified
-    cy.contains(`#date-modified`, new Date(data.dateModified).toLocaleDateString(`en-CA`))
-
-    // # of Lexical Entries
-    cy.contains(`#num-lexical-entries`, count)
-
-    // Projects
-    cy.get(`#projects`).children()
-    .should(`have.length`, data.projects.length)
-    .then(([a, b]) => {
-      expect(a).to.have.text(`Chitimacha Dictionary Project`)
-      expect(b).to.have.text(`Typology Project`)
-    })
-
-    // Notes
-    for (const note of data.notes) {
-      cy.contains(`.note__source`, note.source.abbreviation)
-      cy.contains(`.note__text`, note.text)
-    }
 
   })
 
--- pages/Languages/Languages.test.js
+++ pages/Languages/Languages.test.js
@@ -1,107 +1,234 @@
-import { hasAccess } from '../../utilities/permissions.js'
-import yamlParser    from 'js-yaml'
+import yamlParser from 'js-yaml'
+
+import Language    from '../../models/Language.js'
+import Permissions from '../../models/Permissions.js'
+import Project     from '../../models/Project.js'
 
 const msAuthCookie = Cypress.env(`msAuthCookie`)
+const msAuthUser   = Cypress.env(`msAuthUser`)
 
-describe(`Languages`, function() {
+describe.only(`Languages`, function() {
 
   before(function() {
-
     cy.task(`setupDatabase`)
-    cy.task(`seedDatabase`)
-
-    cy.readFile(`data/languages.yml`)
-    .then(yaml => yamlParser.load(yaml))
-    .as(`languages`)
-
   })
 
-  after(function() {
-    cy.task(`deleteDatabase`)
+  this.afterEach(function() {
+    cy.clearDatabase()
   })
 
   describe(`/languages`, function() {
 
+    it(`displays language data`, function() {
+
+      cy.readFile(`data/language.yml`)
+      .then(yaml => yamlParser.load(yaml))
+      .then(data => {
+        cy.addOne(data)
+        cy.visit(`/languages`)
+        cy.get(`.privacy`).should(`include.text`, `public`)
+        cy.get(`.permissions`).should(`include.text`, `public`)
+        cy.get(`.name`).should(`include.text`, data.name.eng)
+        cy.get(`.autonym`).should(`include.text`, data.autonym.Modern)
+        cy.get(`.abbreviation`).should(`include.text`, data.abbreviation)
+        cy.get(`.glottocode`).should(`include.text`, data.glottocode)
+        cy.get(`.iso`).should(`include.text`, data.iso)
+        cy.get(`.date-created`).should(`include.text`, new Date(data.dateCreated).toLocaleDateString(`en-CA`))
+        cy.get(`.date-modified`).should(`include.text`, new Date(data.dateModified).toLocaleDateString(`en-CA`))
+      })
+
+    })
+
     it(`displays all public languages`, function() {
 
-      const publicLanguages = this.languages.filter(language => language.permissions.public)
+      const count           = 3
+      const publicLanguage  = new Language
+      const privateLanguage = new Language
+
+      privateLanguage.permissions.public = false
+
+      cy.addMany(count, publicLanguage)
+      cy.addMany(count, privateLanguage)
 
       cy.visit(`/languages`)
       cy.title().should(`eq`, `Oxalis | Languages`)
       cy.get(`.page-title`).should(`have.text`, `Languages`)
       cy.get(`.page-nav`).should(`not.exist`)
-      cy.get(`.languages-table tbody`).children().should(`have.length`, publicLanguages.length)
-      cy.contains(`td`, `Chitimacha`).should(`not.exist`)
-      cy.contains(`td`, `Swahili`).should(`not.exist`)
+
+      cy.get(`.languages-table tbody`).children().should(`have.length`, count)
       cy.get(`tbody .privacy`).each(td => expect(td).to.contain(`public`))
       cy.get(`tbody .permissions`).each(td => expect(td).to.contain(`public`))
 
     })
 
-    it(`displays all private languages the user has permission to access (owner@digitallinguistics.io)`, function() {
+    it(`private languages (owner@digitallinguistics.io)`, function() {
 
-      const ownerLanguages = this.languages.filter(language => hasAccess(`owner@digitallinguistics.io`, language))
+      const publicLanguage       = new Language
+      const privateLanguage      = new Language
+      const unauthorizedLanguage = new Language
+
+      privateLanguage.permissions.public = false
+      privateLanguage.permissions.owners.push(msAuthUser)
+      unauthorizedLanguage.permissions.public = false
+
+      const count = 3
+
+      cy.addMany(count, publicLanguage)
+      cy.addMany(count, privateLanguage)
+      cy.addMany(count, unauthorizedLanguage)
 
       cy.visit(`/languages`)
-      cy.setCookie(msAuthCookie, `owner@digitallinguistics.io`)
-      cy.visit(`/languages`)
-      cy.get(`.languages-table tbody`).children().should(`have.length`, ownerLanguages.length)
+      cy.setCookie(msAuthCookie, msAuthUser)
+      cy.reload()
 
-      cy.contains(`td`, `Chitimacha`).parent()
-      .within(() => {
-        cy.get(`.privacy`).should(`include.text`, `private`)
-        cy.get(`.permissions`).should(`include.text`, `owner`)
-      })
-
-      cy.contains(`td`, `Menominee`).parent()
-      .within(() => {
-        cy.get(`.privacy`).should(`include.text`, `public`)
-        cy.get(`.permissions`).should(`include.text`, `owner`)
-      })
+      cy.get(`.languages-table tbody`).children().should(`have.length`, count * 2)
+      cy.get(`tbody .privacy[title=public]`).should(`have.length`, count)
+      cy.get(`tbody .privacy[title=private]`).should(`have.length`, count)
+      cy.get(`tbody .permissions[title=public]`).should(`have.length`, count)
+      cy.get(`tbody .permissions[title=owner]`).should(`have.length`, count)
 
     })
 
-    it(`displays all private languages the user has permission to access (editor@digitallinguistics.io)`, function() {
+    it(`private languages (editor@digitallinguistics.io)`, function() {
 
-      const editorLanguages = this.languages.filter(language => hasAccess(`editor@digitallinguistics.io`, language))
+      const user                 = `editor@digitallinguistics.io`
+      const publicLanguage       = new Language
+      const privateLanguage      = new Language
+      const unauthorizedLanguage = new Language
+
+      privateLanguage.permissions.public = false
+      privateLanguage.permissions.editors.push(user)
+      unauthorizedLanguage.permissions.public = false
+
+      const count = 3
+
+      cy.addMany(count, publicLanguage)
+      cy.addMany(count, privateLanguage)
+      cy.addMany(count, unauthorizedLanguage)
 
       cy.visit(`/languages`)
-      cy.setCookie(msAuthCookie, `editor@digitallinguistics.io`)
-      cy.visit(`/languages`)
-      cy.get(`.languages-table tbody`).children().should(`have.length`, editorLanguages.length)
+      cy.setCookie(msAuthCookie, user)
+      cy.reload()
 
-      cy.contains(`td`, `Menominee`).parent()
-      .within(() => {
-        cy.get(`.privacy`).should(`include.text`, `public`)
-        cy.get(`.permissions`).should(`include.text`, `editor`)
-      })
-
-      cy.contains(`td`, `Ojibwe`).parent()
-      .within(() => {
-        cy.get(`.privacy`).should(`include.text`, `public`)
-        cy.get(`.permissions`).should(`include.text`, `public`)
-      })
+      cy.get(`.languages-table tbody`).children().should(`have.length`, count * 2)
+      cy.get(`tbody .privacy[title=public]`).should(`have.length`, count)
+      cy.get(`tbody .privacy[title=private]`).should(`have.length`, count)
+      cy.get(`tbody .permissions[title=public]`).should(`have.length`, count)
+      cy.get(`tbody .permissions[title=editor]`).should(`have.length`, count)
 
     })
 
-    it(`displays all private languages the user has permission to access (viewer@digitallinguistics.io)`, function() {
+    it(`private languages (viewer@digitallinguistics.io)`, function() {
 
-      const viewerLanguages = this.languages.filter(language => hasAccess(`viewer@digitallinguistics.io`, language))
+      const user                 = `viewe@digitallinguistics.io`
+      const publicLanguage       = new Language
+      const privateLanguage      = new Language
+      const unauthorizedLanguage = new Language
+
+      privateLanguage.permissions.public = false
+      privateLanguage.permissions.viewers.push(user)
+      unauthorizedLanguage.permissions.public = false
+
+      const count = 3
+
+      cy.addMany(count, publicLanguage)
+      cy.addMany(count, privateLanguage)
+      cy.addMany(count, unauthorizedLanguage)
 
       cy.visit(`/languages`)
-      cy.setCookie(msAuthCookie, `viewer@digitallinguistics.io`)
-      cy.visit(`/languages`)
-      cy.get(`.languages-table tbody`).children().should(`have.length`, viewerLanguages.length)
+      cy.setCookie(msAuthCookie, user)
+      cy.reload()
 
-      cy.contains(`td`, `Chitimacha`).parent()
-      .within(() => {
-        cy.get(`.privacy`).should(`include.text`, `private`)
-        cy.get(`.permissions`).should(`include.text`, `viewer`)
-      })
+      cy.get(`.languages-table tbody`).children().should(`have.length`, count * 2)
+      cy.get(`tbody .privacy[title=public]`).should(`have.length`, count)
+      cy.get(`tbody .privacy[title=private]`).should(`have.length`, count)
+      cy.get(`tbody .permissions[title=public]`).should(`have.length`, count)
+      cy.get(`tbody .permissions[title=viewer]`).should(`have.length`, count)
 
     })
 
   })
+
+  describe(`/projects/{projectID}/languages`, function() {
+
+    const badID = `bad-id`
+
+    it(`Project: Not Found`, function() {
+      cy.visit(`/projects/${ badID }/languages`, { failOnStatusCode: false })
+      cy.contains(`.page-title`, `404: Item Not Found`)
+      cy.contains(`.error-message`, `No project exists with ID ${ badID }`)
+    })
+
+    it(`Project: Unauthenticated`, function() {
+
+      const project = new Project({
+        id:          `137a795b-c128-44fa-839a-6d431faccca7`,
+        permissions: new Permissions({ public: false }),
+      })
+
+      const language = new Language({
+        projects: [project.id],
+      })
+
+      cy.addOne(project)
+      cy.addOne(language)
+      cy.visit(`/projects/${ project.id }/languages`, { failOnStatusCode: false })
+      cy.contains(`.page-title`, `401: Unauthenticated`)
+      cy.contains(`.error-message`, `You must be logged in to view this project.`)
+
+    })
+
+    it(`Project: Unauthorized`, function() {
+
+      const project = new Project({
+        id:          `137a795b-c128-44fa-839a-6d431faccca7`,
+        permissions: new Permissions({
+          public: false,
+        }),
+      })
+
+      const language = new Language({
+        projects: [project.id],
+      })
+
+      cy.addOne(project)
+      cy.addOne(language)
+      cy.visit(`/`)
+      cy.setCookie(msAuthCookie, msAuthUser)
+      cy.visit(`/projects/${ project.id }/languages`, { failOnStatusCode: false })
+      cy.contains(`.page-title`, `403: Unauthorized`)
+      cy.contains(`.error-message`, `You do not have permission to view this project.`)
+
+    })
+
+    it(`displays project languages`, function() {
+
+      const project = new Project({
+        id: `a4df0d8e-3456-42fd-97c1-1133757ae872`,
+      })
+
+      const projectLanguage = new Language({
+        projects: [project.id],
+      })
+
+      const otherLanguage = new Language
+
+      const count = 3
+
+      cy.addOne(project)
+      cy.addMany(count, projectLanguage)
+      cy.addMany(count, otherLanguage)
+
+      cy.visit(`/projects/${ project.id }/languages`)
+      cy.get(`.languages-table tbody`).children().should(`have.length`, count)
+
+    })
+
+  })
+
+})
+
+describe(`Languages (OLD)`, function() {
 
   describe(`/projects/{projectID}/languages`, function() {
 
