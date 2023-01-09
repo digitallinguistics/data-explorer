133c58fa2cc10a7ccff0f4fa0a6d3a37d03e8722
Daniel W. Hieber <dwhieb@gmail.com>
2023-01-08 18:26:21 -0700

    Projects

--- pages/Projects/Projects.test.js
+++ pages/Projects/Projects.test.js
@@ -1,61 +1,91 @@
+import yamlParser from 'js-yaml'
+
+import Permissions from '../../models/Permissions.js'
+import Project     from '../../models/Project.js'
+
 const msAuthCookie = Cypress.env(`msAuthCookie`)
+const msAuthUser   = Cypress.env(`msAuthUser`)
 
 describe(`Projects`, function() {
 
-  it(`displays all public projects`, function() {
+  before(function() {
+    cy.task(`setupDatabase`)
+  })
+
+  afterEach(function() {
+    cy.clearDatabase()
+  })
+
+  it(`200: OK (public projects)`, function() {
+
+    const projects = [
+      { name: `Chitimacha Dictionary` },
+      { name: `Nisinoon` },
+      { name: `Typology Project` },
+    ]
+
+    for (const project of projects) {
+      cy.addOne(new Project(project))
+    }
+
     cy.visit(`/projects`)
     cy.title().should(`eq`, `Oxalis | Projects`)
     cy.get(`.page-title`).should(`have.text`, `Projects`)
     cy.get(`.page-nav`).should(`not.exist`)
+    cy.get(`.projects-list`).children().should(`have.length`, projects.length)
+
+    for (const project of projects) {
+      cy.contains(`header`, project.name)
+    }
+
+  })
+
+  it(`200: OK (private projects)`, function() {
+
+    // SETUP
+
+    const publicProject = new Project
+
+    const userProject = new Project({
+      permissions: new Permissions({
+        owners: [msAuthUser],
+        public: false,
+      }),
+    })
+
+    const privateProject = new Project({
+      permissions: new Permissions({
+        public: false,
+      }),
+    })
+
+    cy.addOne(publicProject)
+    cy.addOne(userProject)
+    cy.addOne(privateProject)
+
+    cy.visit(`/projects`)
     cy.get(`.projects-list`).children().should(`have.length`, 1)
-    cy.contains(`header`, `Nisinoon`)
-  })
-
-  it(`displays all private projects the user has permission to access (owner)`, function() {
-    cy.visit(`/projects`)
-    cy.setCookie(msAuthCookie, `owner@digitallinguistics.io`)
-    cy.visit(`/projects`)
-    cy.get(`.projects-list`).children().should(`have.length`, 4)
-    cy.contains(`header`, `Chitimacha`)
-    cy.contains(`header`, `Menominee`)
-    cy.contains(`header`, `Nisinoon`)
-    cy.contains(`header`, `Swahili`)
-  })
-
-  it(`displays all private projects the user has permission to access (editor)`, function() {
-    cy.visit(`/projects`)
-    cy.setCookie(msAuthCookie, `editor@digitallinguistics.io`)
-    cy.visit(`/projects`)
+    cy.setCookie(msAuthCookie, msAuthUser)
+    cy.reload()
     cy.get(`.projects-list`).children().should(`have.length`, 2)
-    cy.contains(`header`, `Menominee`)
-    cy.contains(`header`, `Nisinoon`)
+
   })
 
-  it(`displays all private projects the user has permission to access (viewer)`, function() {
-    cy.visit(`/projects`)
-    cy.setCookie(msAuthCookie, `viewer@digitallinguistics.io`)
-    cy.visit(`/projects`)
-    cy.get(`.projects-list`).children().should(`have.length`, 2)
-    cy.contains(`header`, `Chitimacha`)
-    cy.contains(`header`, `Nisinoon`)
-  })
+  it(`project details`, function() {
 
-  it(`displays all projects for a language`, function() {
+    cy.readFile(`data/project.yml`)
+    .then(yaml => yamlParser.load(yaml))
+    .then(project => {
 
-    const menomineeLanguageID = `5fc405aa-a1a3-41e5-a80d-adb9dfbaa293`
+      cy.addOne(project)
+      cy.visit(`/projects`)
 
-    cy.visit(`/`)
-    cy.setCookie(msAuthCookie, `owner@digitallinguistics.io`)
-    cy.visit(`/languages/${ menomineeLanguageID }/projects`)
+      cy.get(`[data-id=${ project.id }]`).within(() => {
+        cy.contains(`.project__header`, project.name)
+        cy.contains(`.date-created`, `Created ${ new Date(project.dateCreated).toLocaleDateString(undefined, { dateStyle: `long` }) }`)
+      })
 
-    // check for correct page nav
-    cy.contains(`.page-nav`, `Language`)
-    cy.contains(`.page-nav`, `Lexicon`)
-    cy.contains(`.page-nav`, `Projects`)
-
-    cy.get(`.projects-list`).children().should(`have.length`, 2)
-    cy.contains(`.project__header`, `Menominee Dictionary`)
-    cy.contains(`.project__header`, `Nisinoon`)
+    })
 
   })
 
