dcc470ca34dc1fbd2a99002ede583858927a80f5
Daniel W. Hieber <dwhieb@gmail.com>
2023-01-10 03:47:41 -0700

    DEV: fixing tests on CI

--- database/startDatabase.js
+++
@@ -1,8 +0,0 @@
-import { exec }          from '@actions/exec'
-import { fileURLToPath } from 'url'
-import path              from 'path'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname  = path.dirname(__filename)
-
-await exec(`powershell`, [`-NoProfile`, `-ExecutionPolicy`, `Unrestricted`, `-Command`, `-f`, path.resolve(__dirname, `./start.ps1`)])
