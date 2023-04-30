f0460c17f34a84890a570f0d1bb23c28dc4967e1
Daniel W. Hieber <dwhieb@gmail.com>
2023-04-30 14:53:25 -0500

    DEV: add database name to environment variables

--- bin/setupDatabase.js
+++ bin/setupDatabase.js
@@ -1,12 +1,7 @@
 import '../env.js'
 import Database from '@digitallinguistics/db'
 
-const [, , dbName] = process.argv
-
-if (!dbName) {
-  throw new Error(`Provide a database name as the first argument.`)
-}
-
+const dbName   = process.env.COSMOS_DB_NAME
 const endpoint = process.env.COSMOS_ENDPOINT
 const key      = process.env.COSMOS_KEY
 const db       = new Database({ dbName, endpoint, key })
